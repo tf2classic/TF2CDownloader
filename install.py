641920960958f510d858df889ae1a3f219add14a
Chloe <46742114+chloecormier@users.noreply.github.com>
2022-06-03 11:59:52 -0500

    Update install.py
    Pointing to a new download for the meta4 file, removing the codeblock responsible for renaming the zip which should be named consistently now

--- install.py
+++ install.py
@@ -9,15 +9,7 @@ def tf2c_download():
 	gui.message("Starting the download for TF2Classic... You may see some errors that are safe to ignore.")
 	run([vars.ARIA2C_BINARY, "--optimize-concurrent-downloads=true", "--check-certificate=false", "--allow-overwrite=true", "--auto-file-renaming=false", "--continue=true", "--console-log-level=error", "--summary-interval=0", "--bt-hash-check-seed=false", "--seed-time=0",
 	"-d" + vars.TEMP_PATH,
-	"https://tf2classic.org/tf2c/tf2classic.meta4"], check=True)
-	
-	# find whatever zip file it made and rename it
-	
-	dircontent = os.listdir(vars.TEMP_PATH)
-	for file in dircontent:
-		if file.endswith(".zip") and os.path.isfile(vars.TEMP_PATH + file):
-			os.rename(vars.TEMP_PATH + file, vars.TEMP_PATH + "tf2classic.zip")
-			break;
+	"https://wiki.tf2classic.com/misc/tf2classic-latest.meta4"], check=True)
 
 def tf2c_extract():
 	gui.message("Extracting the downloaded archive...")
@@ -25,6 +17,6 @@ def tf2c_extract():
 	if not vars.keepzip:
 		if os.path.isfile(vars.TEMP_PATH + "tf2classic.zip"):
 			os.remove(vars.TEMP_PATH + "tf2classic.zip")
-		if os.path.isfile(vars.TEMP_PATH + "tf2classic.meta4"):
-			os.remove(vars.TEMP_PATH + "tf2classic.meta4")
+		if os.path.isfile(vars.TEMP_PATH + "tf2classic-latest.meta4"):
+			os.remove(vars.TEMP_PATH + "tf2classic-latest.meta4")
 		os.rmdir(vars.TEMP_PATH)
