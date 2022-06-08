0c8e100b3022bc18b0820358d7b6f650a40d2b0f
Chloe <46742114+chloecormier@users.noreply.github.com>
2022-06-08 09:00:46 -0500

    Basic exception handling on the main function list

--- TF2CDownloader.py
+++ TF2CDownloader.py
@@ -25,12 +25,18 @@ def sanity_check():
 	if not stdin or not stdin.isatty():
 		print("Looks like we're running in the background. We don't want that, so we're exiting.")
 		exit(1)
-
-sanity_check()
-setup.setup_path()
-setup.setup_binaries()
-install.free_space_check()
-install.tf2c_download()
-install.tf2c_extract()
+try:
+	sanity_check()
+	setup.setup_path()
+	setup.setup_binaries()
+	install.free_space_check()
+	install.tf2c_download()
+	install.tf2c_extract()
+except:
+	traceback.print_exc()
+	print("----- Exception details above this line -----")
+	print("The program has failed. Post a screenshot in #technical-issues on the Discord.")
+	input("Press Enter to exit.")
+	exit(1)
 
 gui.message_end("The installation has successfully completed. Remember to restart Steam!", 0)
