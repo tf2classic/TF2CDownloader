27020c2689305e5a02334e0ee0be58a0c6bd3c38
Chloe Cormier <christophercormier@protonmail.com>
2022-06-15 13:47:18 -0500

    Consistent indentation

--- setup.py
+++ setup.py
@@ -50,33 +50,33 @@ def sourcemods_path():
             return None
 
 def set_sourcemods_path():
-	"""
-	Set sourcemod folder path.
-	"""
-	if not vars.MAKE_SYMLINK:
-		return 
-	
-	try:
-		src = vars.SOURCEMODS_PATH
-		dest = vars.DEFAULT_SOURCEMODS_PATH
-		
-		if vars.MOVE_TF2CLASSIC_FOLDER:
-			src += "/tf2classic"
-			dest += "/tf2classic"
-		
-		# attempt to remove empty folder, otherwise it'll whine about it
-		if os.path.isdir(dest):
-			os.rmdir(dest)
-		
-		# do the symlink
-		os.symlink(src, dest)
-	except Exception:
-		print("Warning: could not create symlink. is the program running with elevated permissions? This only means TF2Classic will have trouble showing up on Steam.");
-	
+    """
+    Set sourcemod folder path.
+    """
+    if not vars.MAKE_SYMLINK:
+        return 
+    
+    try:
+        src = vars.SOURCEMODS_PATH
+        dest = vars.DEFAULT_SOURCEMODS_PATH
+        
+        if vars.MOVE_TF2CLASSIC_FOLDER:
+            src += "/tf2classic"
+            dest += "/tf2classic"
+        
+        # attempt to remove empty folder, otherwise it'll whine about it
+        if os.path.isdir(dest):
+            os.rmdir(dest)
+        
+        # do the symlink
+        os.symlink(src, dest)
+    except Exception:
+        print("Warning: could not create symlink. is the program running with elevated permissions? This only means TF2Classic will have trouble showing up on Steam.");
+    
 def setup_default_path():
-	vars.DEFAULT_SOURCEMODS_PATH = sourcemods_path()
-	if vars.DEFAULT_SOURCEMODS_PATH is not None:
-		vars.DEFAULT_SOURCEMODS_PATH = sourcemods_path().rstrip('\"')
+    vars.DEFAULT_SOURCEMODS_PATH = sourcemods_path()
+    if vars.DEFAULT_SOURCEMODS_PATH is not None:
+        vars.DEFAULT_SOURCEMODS_PATH = sourcemods_path().rstrip('\"')
 
 def setup_path(manual_path):
     """
@@ -84,47 +84,47 @@ def setup_path(manual_path):
     """
     confirm = False
 
-	smodsfound = isinstance(vars.DEFAULT_SOURCEMODS_PATH, str)
-	if smodsfound and (not manual_path):
-		vars.MOVE_TF2CLASSIC_FOLDER = False
-		vars.MAKE_SYMLINK = False
+    smodsfound = isinstance(vars.DEFAULT_SOURCEMODS_PATH, str)
+    if smodsfound and (not manual_path):
+        vars.MOVE_TF2CLASSIC_FOLDER = False
+        vars.MAKE_SYMLINK = False
 
-		gui.message('Sourcemods folder was automatically found at: ' + vars.DEFAULT_SOURCEMODS_PATH)
-		if gui.message_yes_no('It\'s the recommended installation location. Would you like to install TF2Classic there?'):
-			vars.SOURCEMODS_PATH = vars.DEFAULT_SOURCEMODS_PATH
-			confirm = True
-		else:
-			# check if any sourcemods exists there
-			no_sourcemods = True
-			if os.path.exists(vars.DEFAULT_SOURCEMODS_PATH):
-				for file in os.listdir(vars.DEFAULT_SOURCEMODS_PATH):
-					# there's something in the sourcemods folder, will have to assume it isn't empty
-					no_sourcemods = False
-					break
+        gui.message('Sourcemods folder was automatically found at: ' + vars.DEFAULT_SOURCEMODS_PATH)
+        if gui.message_yes_no('It\'s the recommended installation location. Would you like to install TF2Classic there?'):
+            vars.SOURCEMODS_PATH = vars.DEFAULT_SOURCEMODS_PATH
+            confirm = True
+        else:
+            # check if any sourcemods exists there
+            no_sourcemods = True
+            if os.path.exists(vars.DEFAULT_SOURCEMODS_PATH):
+                for file in os.listdir(vars.DEFAULT_SOURCEMODS_PATH):
+                    # there's something in the sourcemods folder, will have to assume it isn't empty
+                    no_sourcemods = False
+                    break
 
-			if no_sourcemods:
-				if gui.message_yes_no('Then, would you like to move the sourcemods folder location?'):
-					vars.SOURCEMODS_PATH = gui.message_dir('Please enter the location of the new sourcemods folder')
-					vars.MAKE_SYMLINK = True
-				else:
-					setup_path(True)
-					return
-			else:
-				setup_path(True)
-				return
+            if no_sourcemods:
+                if gui.message_yes_no('Then, would you like to move the sourcemods folder location?'):
+                    vars.SOURCEMODS_PATH = gui.message_dir('Please enter the location of the new sourcemods folder')
+                    vars.MAKE_SYMLINK = True
+                else:
+                    setup_path(True)
+                    return
+            else:
+                setup_path(True)
+                return
     else:
-		vars.MOVE_TF2CLASSIC_FOLDER = True
-		vars.MAKE_SYMLINK = smodsfound
+        vars.MOVE_TF2CLASSIC_FOLDER = True
+        vars.MAKE_SYMLINK = smodsfound
 
-		msg = 'Would you like to install in ' + os.getcwd() + '?'
+        msg = 'Would you like to install in ' + os.getcwd() + '?'
 
-		if not smodsfound:
-			gui.message('WARNING: Steam\'s sourcemods folder has not been found.')
-			msg += " You must move it to your sourcemods manually."
-		else:
-			gui.message('WARNING: You chose not to use the Steam\'s sourcemods folder.')
+        if not smodsfound:
+            gui.message('WARNING: Steam\'s sourcemods folder has not been found.')
+            msg += " You must move it to your sourcemods manually."
+        else:
+            gui.message('WARNING: You chose not to use the Steam\'s sourcemods folder.')
 
-		if gui.message_yes_no(msg):
+        if gui.message_yes_no(msg):
             vars.SOURCEMODS_PATH = os.getcwd()
             confirm = True
         else:
