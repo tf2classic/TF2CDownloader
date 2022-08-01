c199841f8404b95af2f9d61f448bc679c5821ecf
Technochips <thetechnochips@protonmail.com>
2022-08-01 03:49:59 +0200

    fixed evil hidden syntax bug

--- troubleshoot.py
+++ troubleshoot.py
@@ -7,5 +7,5 @@ def apply_blacklist():
     gui.message(lang["troubleshoot_blacklist"])
     try:
         urllib.request.urlretrieve("https://wiki.tf2classic.com/serverlist/blacklist.php", vars.INSTALL_PATH + "/tf2classic/cfg/server_blacklist.txt")
-    except e:
+    except:
         gui.message(lang["troubleshoot_blacklist_fail"])
