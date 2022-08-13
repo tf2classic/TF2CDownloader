fab5ab514a0c5b32ddd25a0b7bc410afc1e0191e
Chloe <46742114+chloecormier@users.noreply.github.com>
2022-08-13 23:07:28 +0000

    Temporary mirror for the blacklist

--- troubleshoot.py
+++ troubleshoot.py
@@ -6,6 +6,6 @@ import urllib.request
 def apply_blacklist():
     gui.message(_("Applying safety blacklist..."))
     try:
-        urllib.request.urlretrieve("https://wiki.tf2classic.com/serverlist/blacklist.php", vars.INSTALL_PATH + "/tf2classic/cfg/server_blacklist.txt")
+        urllib.request.urlretrieve("https://tf2classic.org/serverlist/blacklist.php", vars.INSTALL_PATH + "/tf2classic/cfg/server_blacklist.txt")
     except:
         gui.message(_("WARNING: could not apply safety blacklist."))
