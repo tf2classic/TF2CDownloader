1de69617dc26c6313186cd386b835dbe3be36862
Technochips <thetechnochips@protonmail.com>
2022-09-21 15:32:36 +0200

    generating .pot also updates all .po files

--- GeneratePOT.sh
+++ GeneratePOT.sh
@@ -1,1 +1,7 @@
-xgettext --package-name=tf2c-downloader --keyword=_ --keyword=_N:1,2 -l python -o locale/tf2c-downloader.pot *.py
+#!/bin/sh
+POT_FILE=locale/tf2c-downloader.pot
+xgettext --package-name=tf2c-downloader --keyword=_ --keyword=_N:1,2 -l python -o "$POT_FILE" *.py
+for PO_FILE in locale/*/LC_MESSAGES/*.po
+do
+	msgmerge -U "$PO_FILE" "$POT_FILE"
+done
